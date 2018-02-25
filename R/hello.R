3de418ef867505f52caa43f63f64f786a027ff16
Divad0175 <dgranjon@ymail.com>
2018-02-25 15:20:40 +0100

    properly set up the repository.

---
+++ R/hello.R
@@ -0,0 +1,19 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+launch_application <- function() {
+  shiny::runApp(appDir = system.file("App", "example", package = "shinyCV"),
+                display.mode = "normal")
+}
